09d4931cdbb357f3066258c1b69e7503f8eb4f7c
ELSHOYA <hyamalsdyq2@gmail.com>
2023-11-05 09:03:10 +0200

    no

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
-    return my_list
-        
+def element_at(my_list, idx):
+    my_list_len = len(my_list)
+    if idx < 0 or idx > my_list_len - 1:
+        return 'None'
+    else:
+        return my_list[idx]       
